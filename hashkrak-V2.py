df9b143e4056af6ef7b61a57befa0fcc133c15d6
Ryndebstin <61210530+Ryndebstin@users.noreply.github.com>
2020-05-31 13:12:02 +0200

    Update hashkrak-V2.py

--- hashkrak-V2.py
+++ hashkrak-V2.py
@@ -13,5 +13,4 @@ hash=input('type hash : ')
 for i in range(0,len(liste) + 1):
 	if hashlib.sha256(liste[i][0:-1].encode('utf-8')).hexdigest() == hash :
 		print("password : " + liste[i])
-    	exit
 print("no password")
